349f85d87584bf2fd3beaded8c83439cb1884e74
Ivan Kovach <eeiprex@gmail.com>
2023-08-10 16:24:44 +0300

    [add#UnixTools/wc] Header `wc.h` + add example for `wc.c`
    Also add docs, email and version of program in "--help" annotation.

--- src/UnixTools/wc/wc.c
+++ src/UnixTools/wc/wc.c
@@ -1,8 +1,38 @@
-#include <argp.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "wc.h"
+
+
+const char *argp_program_version = "Version: 0.1";
+const char *argp_program_bug_address = "<covach.qn@gmail.com>";
+
+static char doc[] =
+    "wc - program for counting lines, bytes and words in a file.\n\n"
+    "This is essentially a complete (but slightly reduced in functionality) of the original wc program."
+    "\v\nThis program was written for fun for training purposes.";
+
+
+static int32_t parse_options(int32_t key, char *argument, ArgumentParserState *state);
+
+
+static ArgsOption options[] = {
+    {0, 'd', 0, 0, "Show a dot on sreen", 0},
+    {0},
+};
+
+static Argp argp = {options, parse_options, 0, doc, 0, 0, 0};
+
 
 int main(int argc, char *argv[])
 {
-    return argp_parse(0, argc, argv, 0, 0, 0);
+    return argp_parse(&argp, argc, argv, 0, 0, 0);
+}
+
+
+static int32_t parse_options(int32_t key, char *argument, ArgumentParserState *state)
+{
+    switch (key) {
+        case 'd':
+            puts(".");
+            break;
+    }
+    return 0;
 }
---
+++ src/UnixTools/wc/wc.h
@@ -0,0 +1,12 @@
+#ifndef WC_H
+
+#include <argp.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct argp_option ArgsOption;
+typedef struct argp Argp;
+typedef struct argp_state ArgumentParserState;
+
+#endif  // !WC_H
