6cb9735ae619628669d8d2df335dd8fdcb5e9804
Ivan Kovach <eeiprex@gmail.com>
2023-08-13 06:02:13 +0300

    [refact#UnixTools/wc] Broke the main loop into its component parts
    Now the main program loop is one function - "count_data_from_files".
    Which in its turn, depending on the use of the program, counts
    statistics either from files (arguments) or from stdin.
    
    "count_data_from_files" in turn works with two functions:
    
        * _count_stdin
    
        * _count_files
    
    Which implement the "print_statistics" and "count_statistics_from" interface

--- src/UnixTools/wc/wc.c
+++ src/UnixTools/wc/wc.c
@@ -10,8 +10,12 @@
 extern Counting_t *count_statistics_from(FILE *restrict input, Counting_t *restrict total);
 extern void print_statistics(const Counting_t result, const uint16_t settings, const char *restrict file);
 
+
 static int32_t parse_options(int32_t key, char *restrict argument, ArgumentParserState_t *restrict state);
 
+static int count_data_from_files(Arguments_t *args);
+static int _count_files(const Arguments_t *restrict args, Counting_t *restrict total_result);
+static int _count_stdin(const Arguments_t *restrict args, Counting_t *restrict total_result);
 
 // clang-format off
 static ArgsOption_t options[] = {
@@ -34,54 +38,9 @@ int main(int argc, char *argv[])
     Argp_t argp = {options, parse_options, args_doc, doc, 0, 0, 0};
 
     if (argp_parse(&argp, argc, argv, 0, 0, &arguments) == 0) {
-        Counting_t total_result = {.total_newlines = 0, .total_words = 0, .total_bytes = 0, .max_line_length = 0};
-
-        if (arguments.mode == 0) {
-            arguments.mode = PRI_LWB;
-        }
-
-        if (arguments.files.name != NULL) {
-            const char *prev = NULL;
-            char *file_name;
-
-            size_t count_files = 0;
-
-            while ((file_name = argz_next(arguments.files.name, arguments.files.count, prev))) {
-                FILE *from = NULL;
-
-                if ((from = fopen(file_name, "r")) == NULL) {
-                    perror(file_name);
-                } else {
-                    count_files++;
-
-                    Counting_t *file_stat;
-                    if ((file_stat = count_statistics_from(from, &total_result)) == NULL) {
-                        perror("memory error");
-                        return EXIT_FAILURE;
-                    }
-
-                    print_statistics(*file_stat, arguments.mode, file_name);
-                    free(file_stat);
-                    fclose(from);
-                }
-
-                prev = file_name;
-            }
-
-            if (count_files > 0) {
-                print_statistics(total_result, arguments.mode, "total");
-            }
-
-            free(arguments.files.name);
-        } else {
-            Counting_t *stdout_stat = NULL;
-            if ((stdout_stat = count_statistics_from(stdin, &total_result)) == NULL) {
-                perror("memory error");
-                return EXIT_FAILURE;
-            }
-
-            print_statistics(total_result, arguments.mode, "stdin");
-            free(stdout_stat);
+        int status = count_data_from_files(&arguments);
+        if (status != 0) {
+            return EXIT_FAILURE;
         }
     }
 
@@ -115,5 +74,77 @@ static int32_t parse_options(int32_t key, char *restrict argument, ArgumentParse
         default:
             return ARGP_ERR_UNKNOWN;
     }
+    return 0;
+}
+
+
+static int count_data_from_files(Arguments_t *args)
+{
+    Counting_t total_result = {.total_newlines = 0, .total_words = 0, .total_bytes = 0, .max_line_length = 0};
+
+    if (args->mode == 0) {
+        args->mode = PRI_LWB;
+    }
+
+    int status = 0;
+
+    if (args->files.name != NULL)
+        status = _count_files(args, &total_result);
+    else
+        status = _count_stdin(args, &total_result);
+
+    return status;
+}
+
+
+static int _count_files(const Arguments_t *restrict args, Counting_t *restrict total_result)
+{
+    const char *prev = NULL;
+    char *file_name;
+
+    size_t count_files = 0;
+
+    while ((file_name = argz_next(args->files.name, args->files.count, prev))) {
+        FILE *from = NULL;
+
+        if ((from = fopen(file_name, "r")) == NULL) {
+            perror(file_name);
+        } else {
+            count_files++;
+
+            Counting_t *file_stat;
+            if ((file_stat = count_statistics_from(from, total_result)) == NULL) {
+                perror("memory error");
+                return 1;
+            }
+
+            print_statistics(*file_stat, args->mode, file_name);
+            free(file_stat);
+            fclose(from);
+        }
+
+        prev = file_name;
+    }
+
+    if (count_files > 0) {
+        print_statistics(*total_result, args->mode, "total");
+    }
+
+    free(args->files.name);
+
+    return 0;
+}
+
+
+static int _count_stdin(const Arguments_t *restrict args, Counting_t *restrict total_result)
+{
+    Counting_t *stdout_stat = NULL;
+    if ((stdout_stat = count_statistics_from(stdin, total_result)) == NULL) {
+        perror("memory error");
+        return 1;
+    }
+
+    print_statistics(*total_result, args->mode, "stdin");
+    free(stdout_stat);
     return 0;
 }
