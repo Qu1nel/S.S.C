c9eb7ba7db81644111febdc4032621982490bfea
Ivan Kovach <eeiprex@gmail.com>
2023-08-25 20:38:19 +0300

    [add#HelloWorlds] 'Hello World' example on `C#`

---
+++ src/HelloWorlds/C-sharp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HelloWorld {
+    class HelloWorld {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
